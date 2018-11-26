375cc36da594ed204013bd16c3f5469d21039e18
syedmoosa <bsyed6@gmail.com>
2018-11-26 14:48:08 +0530

    Implemented get public holidays function using country ID

--- moviebuff.go
+++ moviebuff.go
@@ -13,6 +13,7 @@ import (
 	"encoding/json"
 	"errors"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -39,6 +40,7 @@ type Moviebuff interface {
 	GetEntity(id string) (*Entity, error)
 	GetResources(resourceType ResourceType, limit, page int) (*Resources, error)
 	GetCertifications(country string) ([]Certification, error)
+	GetHolidayCalendar(countryID string) (*Calendar, error)
 }
 
 type Config struct {
@@ -289,4 +291,42 @@ func (m *moviebuff) GetCertifications(country string) ([]Certification, error) {
 	}
 
 	return certifications.Data, nil
+}
+
+func (m *moviebuff) GetHolidayCalendar(countryID string) (*Calendar, error) {
+	r, err := prepareRequest(m.HostURL, m.StaticToken, "/holidays/"+countryID)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := new(http.Client).Do(r)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		if res.StatusCode == http.StatusForbidden {
+			return nil, ErrInvalidToken
+		}
+		if res.StatusCode == http.StatusNotFound {
+			return nil, ErrResourceDoesNotExist
+		}
+		return nil, ErrResponseNotReceived
+	}
+
+	calendarResp, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	calendarInfo := new(Calendar)
+	err = json.Unmarshal(calendarResp, calendarInfo)
+	if err != nil {
+		log.Println(string(calendarResp))
+		log.Println(err)
+		return nil, err
+	}
+
+	return calendarInfo, nil
 }
